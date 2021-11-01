18bc352ea9148d60bcf3bad4576f76df9474588f
dementisimus <dementisimus@gmail.com>
2021-11-01 17:58:46 +0100

    Bump aswm version

--- src/main/java/dev/dementisimus/mapcreator/creator/aswm/ASWMDownloads.java
+++ src/main/java/dev/dementisimus/mapcreator/creator/aswm/ASWMDownloads.java
@@ -14,7 +14,7 @@ public class ASWMDownloads {
     public static final String VERSION = "2.6.3-SNAPSHOT";
     public static final String PLUGIN_FILE_NAME = "slimeworldmanager-plugin-" + VERSION + ".jar";
     public static final String CLASS_MODIFIER_FILE_NAME = "slimeworldmanager-classmodifier-" + VERSION + ".jar";
-    private static final String SERVER = "https://dl.rapture.pw/ASWM/pull/78/5e136ebbc62c5ebf1c33063a6c6fe3f48b16f9f7/";
+    private static final String SERVER = "https://dl.rapture.pw/ASWM/pull/83/5daae3c26cc6848014d1f646d4e7575340a9ab20/";
     public static final String PLUGIN_URL = SERVER + PLUGIN_FILE_NAME;
     public static final String CLASS_MODIFIER_URL = SERVER + CLASS_MODIFIER_FILE_NAME;
 }
